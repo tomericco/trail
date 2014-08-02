74bca46e825d85fb5f0a0606680ac9622ec67bf6
Tomer Gabbai <tomergab@wix.com>
2014-08-02 13:16:51 +0300

    Meeting submission logic

--- app/scripts/directives/addBrick.js
+++ app/scripts/directives/addBrick.js
@@ -18,8 +18,41 @@ angular.module('trailApp')
         });
 
         $('.meetingTimePicker').clockpicker({
-            autoclose: true
+          autoclose: true
         });
+
+        scope.participants = [];
+
+        scope.toggleMeetingParticipant = function (event, participant) {
+          var el = $(event.target);
+          el.toggleClass('markedOut');
+
+          var participant = _.find(this.participants, { id: participant.id });
+          participant.attending = true;
+        };
+
+        scope.addMeetingBrick = function () {
+          debugger;
+          var goingParticipants = _.filter(this.participants, function (participant) {
+            return !participant.attending;
+          });
+          var brickObj = {
+            type: 'meeting',
+            content: {
+              day: $(el).find('#meetingDayInput').val(),
+              time: $(el).find('#meetingTimeInput').val(),
+              location: 'Hawaii meeting room',
+              purpose: 'sync',
+              participants: goingParticipants
+            }
+          };
+
+          this.addBrick(brickObj);
+        };
+
+        scope.setMeetingParticipants = function (contributor) {
+          this.participants.push(contributor);
+        };
       }
     };
   });
