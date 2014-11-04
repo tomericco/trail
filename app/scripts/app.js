de8fcc11722cb5a8ae17a84f9dba7c90d355f1df
Tomer Gabbai <tomergab@wix.com>
2014-11-04 23:19:27 +0200

    Support dev backend when locading from localhost

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,19 +11,29 @@ angular.module('trailApp', [
   'ui.bootstrap',
   'ui.utils'
 ]).config(function ($stateProvider, $urlRouterProvider, $logProvider) {
-    $stateProvider.state('trail', {
-      url: '/trail/:trailId',
-      templateUrl: 'views/trail.html',
-      controller: 'TrailCtrl'
-    }).state('home', {
-      url: '/home',
-      templateUrl: 'views/home.html',
-      controller: 'MainCtrl'
-    });
-
-    $logProvider.debugEnabled(true);
-  }).constant('FIREBASE_URI', 'https://trail-app.firebaseio.com/')
-  .constant('TrailStatus', {
-    IN_PROGRESS: 'IN_PROGRESS',
-    DONE: 'DONE'
+  $stateProvider.state('trail', {
+    url: '/trail/:trailId',
+    templateUrl: 'views/trail.html',
+    controller: 'TrailCtrl'
+  }).state('home', {
+    url: '/home',
+    templateUrl: 'views/home.html',
+    controller: 'MainCtrl'
   });
+
+  $logProvider.debugEnabled(true);
+}).provider('FIREBASE_URI', [function () {
+  this.$get = function() {
+    var hostname = window.location.hostname;
+    var firebaseUri = 'https://trail-app.firebaseio.com/';
+
+    if (hostname === 'localhost') {
+      firebaseUri = 'https://trail-app-dev.firebaseio.com/';
+    }
+
+    return firebaseUri;
+  };
+}]).constant('TrailStatus', {
+  IN_PROGRESS: 'IN_PROGRESS',
+  DONE: 'DONE'
+});
