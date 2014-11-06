e0f579a5c81b97b431171538dfc1506e325758e5
Tomer Gabbai <tomergab@wix.com>
2014-11-06 13:47:29 +0200

    Don't process images

--- Gruntfile.js
+++ Gruntfile.js
@@ -158,7 +158,7 @@ module.exports = function (grunt) {
           src: [
             '<%= yeoman.dist %>/scripts/{,*/}*.js',
             '<%= yeoman.dist %>/styles/{,*/}*.css',
-//            '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}', Image names are not changed in html files...
+//            '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
             '<%= yeoman.dist %>/styles/fonts/*'
           ]
         }
