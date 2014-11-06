05833ccb763f5eb0fc263c1b7da734c7edde767a
Tomer Gabbai <tomergab@wix.com>
2014-11-06 08:30:33 +0200

    Production build fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -158,7 +158,7 @@ module.exports = function (grunt) {
           src: [
             '<%= yeoman.dist %>/scripts/{,*/}*.js',
             '<%= yeoman.dist %>/styles/{,*/}*.css',
-            '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+//            '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}', Image names are not changed in html files...
             '<%= yeoman.dist %>/styles/fonts/*'
           ]
         }
@@ -180,7 +180,7 @@ module.exports = function (grunt) {
       html: ['<%= yeoman.dist %>/{,*/}*.html'],
       css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
       options: {
-        assetsDirs: ['<%= yeoman.dist %>']
+        assetsDirs: '<%= yeoman.dist %>'
       }
     },
 
