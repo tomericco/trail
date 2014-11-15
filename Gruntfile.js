e01d398ced41e7687bd11125ab2b9d15373c769e
Tomer Gabbai <tomergab@wix.com>
2014-11-15 12:51:40 +0200

    Change dist folder location

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function (grunt) {
     yeoman: {
       // configurable paths
       app: require('./bower.json').appPath || 'public',
-      dist: 'dist'
+      dist: 'public/dist'
     },
 
     // Watches files for changes and runs tasks based on the changed files
