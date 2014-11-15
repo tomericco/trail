1151887aac6acdfaff1c5ac9f651583e9e2e7456
Tomer Gabbai <tomergab@wix.com>
2014-11-15 15:24:55 +0200

    Load minified client on production

--- app.js
+++ app.js
@@ -4,19 +4,21 @@ var mail = require('./routes/mail');
 var bodyParser = require('body-parser');
 
 var app = express();
+var clientDir = os.environ.TRAIL_ENV === 'production' ? 'public/dist' : 'public';
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, clientDir)));
 
 // Routes
 app.get('/mail', mail.main);
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', process.env.PORT || 3000);
 
 var port = app.get('port');
 var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port)
+  console.log('Example app listening at http://%s:%s', host, port);
 });
