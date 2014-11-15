07865b65c43ee1fc808e8406e6bf0e70f7a637e7
Tomer Gabbai <tomergab@wix.com>
2014-11-15 13:46:00 +0200

    Get port from heroku

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 // Routes
 app.get('/mail', mail.main);
 
-var server = app.listen(3000, function () {
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
 
