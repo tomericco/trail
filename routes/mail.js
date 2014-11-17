4821e36bbc3f621534e52dc2bd8d43f359b7f9fd
Tomer Gabbai <tomergab@wix.com>
2014-11-18 00:39:21 +0200

    Basic SendGrid integration (WIP)

--- routes/mail.js
+++ routes/mail.js
@@ -1,5 +1,19 @@
+var sendgrid  = require('sendgrid')('tomericco', 'Tomer1986');
+
 module.exports = {
     main: function(req, res) {
         res.send('Hello from mail service!');
+
+        var email = new sendgrid.Email({
+            to: 'tomer.gabbai@gmail.com',
+            from: 'no_reply@mytrail.io',
+            subject: 'Mail from Trail!',
+            text: 'Ta-Da !'
+        });
+
+        sendgrid.send(email, function(err, json) {
+            if (err) { return console.error(err); }
+            console.log(json);
+        });
     }
 };
